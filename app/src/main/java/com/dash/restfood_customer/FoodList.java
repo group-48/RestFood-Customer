cf1580845a6e8692f994b76c47698c4a60be3c3d
nawanjana <nkeshalya@gmail.com>
2020-01-11 11:46:21 +0530

    updated foodlist activity

--- app/src/main/java/com/dash/restfood_customer/FoodList.java
+++ app/src/main/java/com/dash/restfood_customer/FoodList.java
@@ -148,6 +148,7 @@ public class FoodList extends BaseActivity {
                 inta.putExtra("Food",obj.getFoodName());
                 inta.putExtra("docId",documentSnapshot.getId());
                 inta.putExtra("shopdoc",docId);
+                inta.putExtra("Browse", getIntent().getStringExtra("Browse"));
                 startActivity(inta);
             }
         });
