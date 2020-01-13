b9a9c287a4c5ed55a88401bed224a017adf917f5
nawanjana <nkeshalya@gmail.com>
2020-01-13 13:58:42 +0530

    Updated reserve model

--- app/src/main/java/com/dash/restfood_customer/models/Reserve.java
+++ app/src/main/java/com/dash/restfood_customer/models/Reserve.java
@@ -5,29 +5,21 @@ import java.util.Date;
 
 public class Reserve {
 
-    private String custName,shopId,shopName;
-    private Integer tableno, guestno ;
+    private String shopId,shopName;
+    private Integer guestno ;
     private Date date;
     private Time time;
 
     public Reserve(String custName, String shopId, String shopName, Integer tableno, Integer guestno, Date date, Time time) {
-        this.custName = custName;
+
         this.shopId = shopId;
         this.shopName = shopName;
-        this.tableno = tableno;
         this.guestno = guestno;
         this.date = date;
         this.time = time;
     }
     public Reserve(){}
 
-    public String getCustName() {
-        return custName;
-    }
-
-    public void setCustName(String custName) {
-        this.custName = custName;
-    }
 
     public String getShopId() {
         return shopId;
@@ -45,13 +37,6 @@ public class Reserve {
         this.shopName = shopName;
     }
 
-    public Integer getTableno() {
-        return tableno;
-    }
-
-    public void setTableno(Integer tableno) {
-        this.tableno = tableno;
-    }
 
     public Integer getGuestno() {
         return guestno;
